f8dcaeba13969da0546cd4ce23f607d97ffe850f
Vincent <nguyenhuuvui89@gmail.com>
2023-06-16 22:51:56 -0230

    working with API in location page

---
+++ Biography_html_css/js/family.js
@@ -0,0 +1,48 @@
+const gallery = document.querySelector('.gallery');
+const galleryBox = gallery.querySelector('.images');
+const imgArr = gallery.querySelectorAll('.item .kid');
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+const dotBar = gallery.querySelectorAll('.dot-bar li');
+
+const imgArrLen = imgArr.length - 1;
+let i = 0;
+let setIntervalTime = setInterval(() => { rightArrow.click(); }, 5000);
+
+// Move image function
+function moveImage() {
+  galleryBox.style.left = `${-imgArr[i].offsetLeft}px`;
+  document.querySelector('.act').classList.remove('act');
+  dotBar[i].classList.add('act');
+  clearInterval(setIntervalTime);
+  setIntervalTime = setInterval(() => { rightArrow.click(); }, 5000);
+}
+
+// Applied event to arrow buttons
+rightArrow.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (i + 1 <= imgArrLen) {
+    i += 1;
+  } else {
+    i = 0;
+  }
+  moveImage();
+});
+
+leftArrow.addEventListener('click', (e) => {
+  e.preventDefault();
+  i -= 1;
+  if (i < 0) {
+    i = imgArrLen;
+  }
+  moveImage();
+});
+
+// Applied move images to dots
+
+for (let index = 0; index <= imgArrLen; ++index) {
+  dotBar[index].addEventListener('click', () => {
+    i = index;
+    moveImage();
+  });
+}
---
+++ Biography_html_css/js/location.js
@@ -0,0 +1,41 @@
+const searchForm = document.getElementById('searchForm');
+const searchBox = document.getElementById('box');
+const searchResult = document.querySelector('.result');
+const btn = document.querySelector('.show-more');
+
+let keyword = '';
+let page = 1;
+// Load API from Unsplash API
+const loadData = async () => {
+  try {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=wggD8lARSaMiJLDJt68h7bzM4yJDyS25Pypqjvt_e_M&per_page=9`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const searchResults = data.results;
+    searchResults.forEach((result) => {
+      const image = document.createElement('img');
+      image.src = result.urls.small;
+      searchResult.appendChild(image);
+    });
+    btn.style.visibility = 'visible';
+  } catch (error) {
+    const h3 = document.querySelector('.error');
+    h3.innerHTML = `There are something wrongs: ${error}`;
+  }
+};
+
+// Add submit into form
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  page = 1;
+  loadData();
+  searchResult.innerHTML = '';
+});
+
+// Add click into button to show more images
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  page++;
+  loadData();
+});
