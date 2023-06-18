9ef37af4abe0fe82c9ebf76192a4ff1b5ffddd9c
Vincent <nguyenhuuvui89@gmail.com>
2023-06-18 17:11:42 -0230

    using Vue render Footer

---
+++ js/footerVue.js
@@ -0,0 +1,23 @@
+const app2 = new Vue({
+  el: '#app2',
+  data: {
+    footer: [
+      {
+        "title": "About",
+        "items": ["Technology", "Programming", "Development", "Database"],
+      },
+      {
+        "title": "Properties",
+        "items": ["Home", "Family", "Education", "Professional", "Contact"],
+      },
+      {
+        "title": "Services",
+        "items": ["Web Development", "Data Warehousing", "IT Management", "API Integration"]
+      },
+      {
+        "title": "About",
+        "items": ["America", "Canada"],
+      },
+    ],
+  },
+});
